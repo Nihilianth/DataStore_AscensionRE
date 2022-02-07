f56d0f0248c16d385798150d61cfd7d900289afa
nihilianth <nihili4nth@gmail.com>
2022-02-07 19:27:01 +0100

    [WIP] Rewrote data acquisition, RE unlock handling. Dropped Addon message parsers which are no longer required.

--- DataStore_AscensionRE/DataStore_AscensionRE.lua
+++ DataStore_AscensionRE/DataStore_AscensionRE.lua
@@ -35,7 +35,8 @@ local MSG_SEND_ENCHANTS 	= 3
 local AddonDB_Defaults = {
     global = {
         Options = {
-            BroadcastREs = 1
+            BroadcastREs = 1,
+            AnnounceInit = 1
         },
         Characters = {
             ['*'] = {
@@ -121,9 +122,9 @@ local function SaveEnchants(sender, alt, data)
     if thisGuild and sender then
         local addonVer = thisGuild.Members[sender].Version
         local member_alt = thisGuild.Members[alt]
-        -- addon:Print("Saving enchant from "..sender)
+        --addon:Print("Saving enchant from "..sender)
         member_alt.Version = addonVer
-        -- addon:Print(format("Gota data for %s: %u", alt, #data))
+        -- addon:Print(format("Got data for %s: %u", alt, #data))
         member_alt.NumKnownEnchants = #data
         member_alt.KnownEnchants = data
         member_alt.lastUpdate = time()
@@ -171,7 +172,6 @@ local function SendEnchantsWithAlts(alts, recipient)
     enchantQueue = enchantQueue or {}
 
     --addon:Print("sending alt enchants")
-    --addon:Print("Alts: " .. alts)
 
     if AscensionInit == true then
         local myself = DataStore:GetCharacter()
@@ -191,78 +191,6 @@ local function SendEnchantsWithAlts(alts, recipient)
     end
 
     enchantTimer = enchantTimer or addon:ScheduleRepeatingTimer(SendEnchantQueue, 0.5) -- Send queued enchants every 0.5 seconds
-end
-
---[[ * True Spell IDs *
-    The CollectionStore contains some variant of skill IDs
-    most of which match with spell IDs. For others true spell IDs
-    has to be retrieved to ensure proper use for (tooltip and other) addon functionality
--- ]]
-local function GetRealRESpellIds()
-    local reList = CollectionsFrame.EnchantList
-    local spellIds = {}
-
-    for reId, misc in pairs(reList) do
-        local spellId = reList[reId][1][2]
-
-        table.insert(spellIds, spellId)
-    end
-
-    return spellIds
-end
-
--- Parse Server Message containing known Enchants using Smallfolk (used by Ascension addons)
-local function ParseKnownEnchantsList(message)
-    -- TODO: Large server messages are split. 
-    -- Ingame tests showed no issues for players with large enchant collections
-    -- Further tests are needed to determine if additional processing is required
-    -- TODO: Additional error handling if required
-
-    local reList = CollectionsFrame.EnchantList
-    local knownList = {}
-    addon:Print("ParseKnownEnchantsList called. Message len "..strlen(message))
-
-    local recvTable = Smallfolk.loads(message, #message)
-    if not recvTable or type(recvTable) ~= 'table' then
-        addon:Print("Error: received invalid enchant data with size: "..#msg)
-        return
-    end
-
-    for id, enchant_id in pairs(recvTable[1][4]) do
-        local spellId = reList[enchant_id][1][2]
-        if not spellId then
-            addon:Print("Unknown enchant with id: "..enchant_id.." skipped.")
-        else
-            table.insert(knownList, spellId)
-        end
-    end
-
-    -- addon:Print("Setting known list for char "..UnitName("player").." size: "..#knownList)
-    addon.ThisCharacter.NumKnownEnchants = #knownList
-    addon.ThisCharacter.KnownEnchants = knownList
-    addon.ThisCharacter.lastUpdate = time()
-    addon.ThisCharacter.Version = GetBuildVersion()
-    addon:SendMessage(collectionUpdateMsgName, knownList, AscensionInit)
-    SendEnchantsWithAlts("") -- send own enchants
-end
-
--- Parse server message received after unlocking a new RE
-local function ParseNewEnchant(message)
-    addon:Print("RE Toolkit: Got FillCollectionByEnchant")
-    local reList = CollectionsFrame.EnchantList
-
-    local recvTable = binser.decode(message, #message)
-    if not recvTable or type(recvTable) ~= 'table' then
-        addon:Print("Received invalid new enchant message. Size: "..#message)
-        addon:Print(message)
-    end
-    
-    local entry = reList[recvTable[1][4]][1][2]
-    if entry then 
-        addon:SendMessage(collectionREUnlockedMsgName, entry)
-    else
-        addon:Print("Received unknown New Enchant!")
-    end
 end
 
 -- verify this
@@ -304,17 +232,8 @@ local GuildCommCallbacks = {
 
 -- *** End Utility Functions Ascension ***
 
-
-
 -- *** Public Functions Impl ***
-
-local function _GetRESpellList()
-    --addon:Print("GetRESpellList called!")
-    if AscensionInit == false then return nil end
-
-    return AscRESpellIds
-end
-
+-- FIXME: old
 local function _GetKnownEnchants()
     if AscensionInit == false then return nil end
 
@@ -376,7 +295,6 @@ local function _GetGuildiesWithRE(entry)
 end
 
 local PublicMethods = {
-    GetRESpellList = _GetRESpellList,
     GetKnownEnchants = _GetKnownEnchants,
     GetCharactersWithRE = _GetCharactersWithRE,
     GetGuildiesWithRE = _GetGuildiesWithRE,
@@ -385,30 +303,77 @@ local PublicMethods = {
 }
 -- *** End Public Functions Impl ***
 
+--- *** AddOns\AscensionUI\MysticEnchant\MysticEnchant.lua ***
 
--- *** WOW Events ***
+-- known = false -> unknown only
+local function BuildKnownList(known)
+    local KnownEnchantCount = 0
+    local list = {}
 
-local function InitAscensionData()
-    AscRESpellIds = AscensionUI.REListSpellID
-    local knownList = {}
-    for enchantID, RE in pairs(AscensionUI.REList) do
-        if RE.enchantID > 0 then
-            spellid = RE.spellID
-            if (IsReforgeEnchantmentKnown(RE.enchantID)) then
-                table.insert(knownList, spellid)
-            end  
+    for i , v in pairs(AscensionUI.REList) do
+        if not v.known then
+            v.known = IsReforgeEnchantmentKnown(i)
+        end
+        if v.known then
+            KnownEnchantCount = KnownEnchantCount + 1
+        end
+        if v.known and known then
+            table.insert(list, v)
+        elseif not known and not v.known then
+            table.insert(list, v)
         end
     end
 
-    --addon:Print("Setting known list for char "..UnitName("player").." size: "..#knownList)
-    addon.ThisCharacter.NumKnownEnchants = #knownList
+    return list, KnownEnchantCount
+end
+
+--[[
+Initialize known REs with the new API
+]]--
+local function InitAscensionData()
+    local knownList, knownListCnt = BuildKnownList(true)
+
+    -- addon:Print("Setting known list for char "..UnitName("player").." size: "..#knownList)
+    addon.ThisCharacter.NumKnownEnchants = knownListCnt
     addon.ThisCharacter.KnownEnchants = knownList
     addon.ThisCharacter.lastUpdate = time()
     addon.ThisCharacter.Version = GetBuildVersion()
-    addon:SendMessage(collectionUpdateMsgName, knownList, AscensionInit)
     SendEnchantsWithAlts("") -- send own enchants
+    -- addon:SendMessage("ASC_COLLECTION_INIT", knownList)
+    addon:SendMessage("ASC_COLLECTION_UPDATE", knownList, AscensionInit)
     AscensionInit = true
 end
+
+function addon:COMMENTATOR_SKIRMISH_QUEUE_REQUEST(event, subevent, data ,...)
+    -- addon:Print(string.format("Custom ASC Event %s", subevent))
+    if subevent == "ASCENSION_REFORGE_ENCHANTMENT_LEARNED" then
+        --MysticEnchant checks for RE validity, prints error
+
+        RE = GetREData(data)
+        if RE.enchantID ~= 0 then
+            table.insert(addon.ThisCharacter.KnownEnchants, RE.spelID)
+            addon.ThisCharacter.NumKnownEnchants = #addon.ThisCharacter.KnownEnchants
+            -- Notify other addons
+            addon:SendMessage("ASC_COLLECTION_RE_UNLOCKED", RE.enchantID)
+            addon:SendMessage("ASC_COLLECTION_UPDATE", addon.ThisCharacter.KnownEnchants, AscensionInit)
+
+            -- This should not occur. Verify?
+            if RE.enchantID ~= data then
+                addon:Print(format("Enchant id mismatch: %s vs %s", RE.enchantID, data))
+            end
+
+            -- Print message
+            local _, _, icon = GetSpellInfo(RE.spellID)
+            texture = CreateTextureMarkup(icon, 64, 64, 64, 64, 0, 1, 0, 1)
+            local enchantColor = AscensionUI.MysticEnchant.EnchantQualitySettings[RE.quality][1]
+            addon:Print(format("%s|Hspell:%s|h%s[%s]|r|h RE learned!", texture, RE.spellID, enchantColor, RE.spellName))
+        end
+    
+    end
+end
+
+-- *** WOW Events ***
+
 function addon:OnInitialize()
 	addon.db = LibStub("AceDB-3.0"):New(addonName .. "DB", AddonDB_Defaults)
 	DataStore:RegisterModule(addonName, addon, PublicMethods)
@@ -422,152 +387,16 @@ function addon:OnInitialize()
 end
 
 function addon:OnEnable()
-    addon:RegisterEvent("CHAT_MSG_ADDON")
+    addon:RegisterEvent("COMMENTATOR_SKIRMISH_QUEUE_REQUEST")
     -- TODO: addon:SetupOptions()
 end
 
 function addon:OnDisable()
-    addon:UnregisterEvent("CHAT_MSG_ADDON")
+    addon:UnregisterEvent("COMMENTATOR_SKIRMISH_QUEUE_REQUEST")
 end
 
 local function SendOnlineInfo()
     if onlineCnt then
-        print(format("Online guild members with DataStore_AscensionRE: %u.", onlineCnt))
-    end
-end
-
--- From https://github.com/Rochet2/AIO/blob/d06da5d8e7c32e45fde93a1632e9e33105dfd895/AIO_Client/AIO.lua
--- Converts an uint16 number to string (2 chars)
--- Note that this escapes using \0 character so the full uint16 range is not usable
-local function AIO_16tostring(uint16)
-    -- split 16bit to 2 8bit parts but without \0
-    assert(uint16 <= 2^16-767, "Too high value")
-    assert(uint16 >= 0, "Negative value")
-    local high = math.floor(uint16 / 254)
-    local l = high +1
-    local r = uint16 - high * 254 +1
-    return string.char(l)..string.char(r)
-end
-
-local recvBuffer = {}
-local enchantMsgId = -1
--- Converts a string (2 chars) to uint16 number
--- Note that the chars can not be \0 character so the full uint16 range is not usable
-local function AIO_stringto16(str)
-    local l = string.byte(string.sub(str, 1,1)) -1
-    local r = string.byte(string.sub(str, 2,2)) -1
-    local val = l*254 + r
-    assert(val <= 2^16-767, "Too high value")
-    assert(val >= 0, "Negative value")
-    return val
-end
-local AIO_ShortMsg          = string.char(1)..string.char(1)
-
-function addon:CHAT_MSG_ADDON(event, prefix, message, channel, sender)
-    
-    if not prefix or not prefix:find("SAI2O") then return end
-    if not message then return end
-
-    -- From AIO documentation
-        -- the chars can not contain \0
-        -- 16bit -> Message ID -- 0 reserved for identifying short msg
-        -- 16bit -> Number of parts (should be > 1)
-        -- 16bit -> Part ID
-        -- Rest -> Message String
-
-
-    local msgid = string.sub(message, 1,2)
-    local messageId = AIO_stringto16(msgid)
-
-    if enchantMsgId > 0 and messageId ~= enchantMsgId then
-        addon:Print("Message ID mismatch"..enchantMsgId.." "..messageId)
-        return
-    end
-
-
-    if msgid == AIO_ShortMsg then
-        -- addon:Print("Short Message")
-        message = string.sub(message, 3)
-    --elseif  message:find("BuildEnchantList") or enchantMsgId == messageId then -- sic!
-    elseif  message:find("GetKnownEnchantsList") or enchantMsgId == messageId then -- sic!
-        -- print("messageId "..messageId)
-        if #message < 3*2 then
-            return
-        end
-        local parts = AIO_stringto16(string.sub(message, 3,4))
-        local partId = AIO_stringto16(string.sub(message, 5,6))
-        if partId <= 0 or partId > parts then
-            print("received long message with invalid amount of parts. id, parts: "..partId.." "..parts)
-            return
-        end
-
-        message = string.sub(message, 7)
-        if parts >= 40 then 
-            addon:Print("Received "..parts.."parts for GetKnownEnchantsList. Aborting")
-            return
-        end
-
-        if not recvBuffer[messageId] then
-            enchantMsgId = messageId
-            --addon:Print("got new message "..messageId)
-            recvBuffer[messageId] = {
-                partsCnt = parts,
-                partsStored = 1,
-                parts = { message }
-            }
-            return
-        end
-        msgBuf = recvBuffer[messageId]
-        --print(string.sub(message, 1, 30), string.sub(message, -30))
-        
-        if msgBuf then
-            table.insert(msgBuf.parts, message)
-            msgBuf.partsStored = msgBuf.partsStored + 1
-
-            if msgBuf.partsCnt == msgBuf.partsStored then
-                --local msgLenCalc = (msgBuf.partsCnt - 1) * 2549 + #message
-                --addon:Print("Received all parts")
-                -- print(string.sub(message, 1, 30), string.sub(message, -30))
-                message = table.concat(msgBuf.parts)
-                --print(msgLenCalc, #message, msgBuf.partsStored)
-                --addon:Print(string.sub(message, 1, 30), " : ", string.sub(message, -30))
-                --addon:Print("found "..string.find(message, "Sanctified Light"))
-                enchantMsgId = -1
-                smallFolkLoad = Smallfolk.loads(message, #message)
-            else
-                --print(messageId, partId, parts, #message)
-                --print(string.sub(message, 1, 30), " : ", string.sub(message, -30))
-                --addon:Print("Saved part "..partId)
-                return
-            end
-        end
-
-    end
-
-    if string.sub(message,1,2) ~= (string.char(1)):rep(2) then
-        local msg_noprefix = string.sub(message, 3)
-        --print("split message "..strlen(message))
-        --print(string.sub(msg_noprefix, 1, 30), string.sub(msg_noprefix, -30))
-    end
-    -- Provided after login as a responce to requests from client addons
-    -- Data is not present on PLAYER_ALIVE
-    if message:find("GetKnownEnchantsList") then
-        addon:Print("Received GetKnownEnchantsList")
-        charEnchants = {}
-
-        if AscensionInit == false then
-            AscRESpellIds = GetRealRESpellIds()
-            addon:SendMessage(collectionInitMsgName, AscRESpellIds)
-            ParseKnownEnchantsList(message)
-            addon:ScheduleTimer(SendOnlineInfo, 10)
-            AscensionInit = true
-        else
-            ParseKnownEnchantsList(message)
-        end
-        
-        addon:Print("Parsed GetKnownEnchantsList")
-    elseif message:find("FillCollectionByEnchant") then
-        ParseNewEnchant(message)
-        addon:Print("Unlocked Enchant")
+        addon:Print(format("Online guild members with DataStore_AscensionRE: %u.", onlineCnt))
     end
 end
